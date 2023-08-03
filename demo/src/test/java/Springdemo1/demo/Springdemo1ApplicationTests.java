9d54e27c2d6228d29928a9505241411c54054f04
VijayMaadhu <vijaymadhu456@gmail.com>
2023-08-03 10:55:26 +0530

    First commit

---
+++ demo/src/test/java/Springdemo1/demo/Springdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Springdemo1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
